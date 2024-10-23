05feb6cad905a33ae976c28aa14c7d0c84dc73a6
RK3nshin <oliveiraguilbert@gmail.com>
2024-10-23 16:06:38 -0300

    Api-Usuario

--- frontend/script.js
+++ frontend/script.js
@@ -1,14 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // URL do banco de dados (substitua pela sua)
     const url = 'http://localhost:9082/usuarios';
 
-    // Faz a requisição fetch
     fetch(url)
         .then(response => response.json())
         .then(data => {
             const tableBody = document.querySelector('#userTable tbody');
             
-            // Loop para adicionar as linhas à tabela
             data.forEach(user => {
                 const row = document.createElement('tr');
                 
